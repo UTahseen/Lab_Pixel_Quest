46a33610256d7472f62f002c47a492f1ce72f19d
UTahseen <tahseenu@nycstudents.net>
2024-12-07 15:40:43 -0500

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+
+//Controls the bullet that can hit player on Enemy 
+public class Bullet : MonoBehaviour
+{
+    //==================================================================================================================
+    // Variables 
+    //==================================================================================================================
+
+    //Used by Player Spawner 
+    private Camera _camera;    //Camera Game Object 
+    private Vector3 _mousePos; //Current Mouse Position  
+
+    //Movement Controls 
+    private Rigidbody2D _rigidbody2D; //The rigidbody that will move the bullet 
+    public float speed;           //Speed at which the bullet moves 
+
+    //Flag and Timer 
+    public float deathTime = 3f;   //How long before the bullet dies 
+
+    //==================================================================================================================
+    // Base Method  
+    //==================================================================================================================
+
+    //Checks who is shooting the bullet and set up the bullet settings 
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        StartCoroutine(Death());
+    }
+
+    //==================================================================================================================
+    // Bullet Set Up  
+    //==================================================================================================================
+
+    private void Update()
+    {
+        SpriteRenderer playerRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        if (playerRenderer.flipX) speed = 15;
+        else speed = -15;
+        transform.position += new Vector3(speed *Time.deltaTime, 0, 0);
+    }
+
+    //Waits till timer is out then destroys the bullet 
+    private IEnumerator Death()
+    {
+        yield return new WaitForSeconds(deathTime);
+        Destroy(gameObject);
+    }
+}
---
+++ RedWalking.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class RedWalking : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;           // Controls players movement 
+    private SpriteRenderer _spriteRenderer;     // Controls player image 
+    public float xSpeed = 4;                    // Speed at which the player moves
+    private int health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        // Get sprite rendered from child object 
+        _spriteRenderer = gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gets player input, -1 for Left Arrow/A, 0 for no key press, 1 for Right Arrow/D 
+        float xInput = Input.GetAxis("Horizontal");
+
+        // Flips the players image based on the direction the player is clicking 
+        if (xInput < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else if (xInput > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+
+        // Put the speed in the player with given key input 
+        _rigidbody2D.velocity = new Vector2(xInput * xSpeed, _rigidbody2D.velocity.y);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bullet")
+        {
+            health -= 5;
+            Destroy(collision.gameObject);
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
